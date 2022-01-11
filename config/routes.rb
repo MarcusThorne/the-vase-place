df1eea4d816bdaba5b10b6b867dbc4d148745f05
Marcus Thorne <marcus_thorne475@hotmail.com>
2022-01-11 11:25:58 -0800

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: "pages#home"
+  root to: 'pages#home'
 
-  get 'basket', to: "basket#show"
-  get "about", to: "pages#about"
-  get "policy", to: "pages#policy"
-  get "account", to: "pages#account"
+  get 'basket', to: 'basket#show'
+  get 'about', to: 'pages#about'
+  get 'policy', to: 'pages#policy'
+  get 'account', to: 'pages#account'
   get 'wishlists/update'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: %i[index show]
   resources :wishlists
   resources :basket_items
-  resource :baskets, only: [:show]
+  resources :charges, only: %i[new create]
+  resource :baskets, only: %i[show]
 end

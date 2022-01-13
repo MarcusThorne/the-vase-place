3a3ba8ad8eafec05dff94d7685fecd676d0132cb
Marcus Thorne <marcus_thorne475@hotmail.com>
2022-01-12 19:02:15 -0800

    made homepage responsive

--- app/javascript/components/carousel.js
+++ app/javascript/components/carousel.js
@@ -5,7 +5,54 @@ const carousel = () => {
     slidesToScroll: 4,
     arrows: true,
     nextArrow: '.next_slide',
-    prevArrow: '.previous_slide'
+    prevArrow: '.previous_slide',
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          arrows: false,
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          centerMode: true,
+          slidesToShow: 2,
+          centerPadding: "0px"
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "110px"
+        }
+      },
+      {
+        breakpoint: 500,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: "70px"
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 }
 

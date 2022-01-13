bbfaef837f8462b920b43269ceba750781ed20bb
Marcus Thorne <marcus_thorne475@hotmail.com>
2022-01-12 19:30:24 -0800

    fixed database errors

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,14 +1,2 @@
 class Basket < ApplicationRecord
-  has_many :basket_items
-  before_save :set_subtotal
-
-  def subtotal
-    basket_items.collect { |basket_item| basket_item.valid? ? basket_item.unit_price * basket_item.quantity : 0 }.sum
-  end
-
-  private
-
-  def set_subtotal
-    self[:subtotal] = subtotal
-  end
 end
--- db/migrate/20211002115236_rename_orders_to_baskets.rb
+++
@@ -1,5 +0,0 @@
-class RenameOrdersToBaskets < ActiveRecord::Migration[6.1]
-  def change
-    rename_table :orders, :baskets
-  end
-end
